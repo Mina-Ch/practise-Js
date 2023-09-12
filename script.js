bca838ad2f21bbc802436d6571750a3c212e7b3a
minacheraghali <minaa.ch76@gmail.com>
2023-09-12 20:22:24 +0330

    update script.js

--- script.js
+++ script.js
@@ -1,47 +1,79 @@
 "use strict";
 
-let firstname = "mina";
-let lastname = "cheraghali";
-let age = "26";
+// let firstname = "mina";
+// let lastname = "cheraghali";
+// let age = "26";
 
-console.log(
-  `my name is ${firstname}, my lastname is ${lastname}, im ${age} years old`
-);
-
-//-------------------------------------//
-
-let surname = "cheraghali";
-let completeName = `my name is mina ${surname}`;
-console.log(completeName);
+// console.log(
+//   `my name is ${firstname}, my lastname is ${lastname}, im ${age} years old`
+// );
 
 //-------------------------------------//
 
-const sum = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(sum(5, 8));
+// let surname = "cheraghali";
+// let completeName = `my name is mina ${surname}`;
+// console.log(completeName);
 
 //-------------------------------------//
 
-const sub = (num1, num2) => {
-  return num1 - num2;
-};
-console.log(sub(15, 6));
+// const sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(sum(5, 8));
 
 //-------------------------------------//
 
-const multi = (num1, num2) => {
-  return num1 * num2;
-};
-console.log(multi(8, 4));
+// const sub = (num1, num2) => {
+//   return num1 - num2;
+// };
+// console.log(sub(15, 6));
 
 //-------------------------------------//
-const div = (num1 = 18, num2 = 2) => {
-  return num1 / num2;
-};
-console.log(div());
 
-const div1 = (num1 = 18, num2 = 2) => {
-  return num1 / num2;
-};
-console.log(div1());
+// const multi = (num1, num2) => {
+//   return num1 * num2;
+// };
+// console.log(multi(8, 4));
+
+//-------------------------------------//
+// const div = (num1 = 18, num2 = 2) => {
+//   return num1 / num2;
+// };
+// console.log(div());
+
+// const div1 = (num1 = 18, num2 = 2) => {
+//   return num1 / num2;
+// };
+// console.log(div1());
+
+//-------------------------------------//
+
+//spread
+
+// let numbers=[1 ,5 ,9 ,16 ,42 ,68];
+
+// console.log(...numbers);
+
+//-------------------------------------//
+
+// let a = ["a" , "b" , "c"]
+// let b = ["d","e","f"]
+
+// let all = [...a , "z" ,...b]
+
+// console.log(all);
+
+//-------------------------------------//
+
+// let user ={
+//   name:"mina",
+//   lastname:"cheraghali",
+//   age:26
+// }
+// let data = {...user}
+// user.age = 27
+
+// console.log(data);
+// console.log(user);
+
+//-------------------------------------//
